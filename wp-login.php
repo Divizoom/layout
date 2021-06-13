459b08d5923ec3c4609385030116d796e774d49a
Divizoom <info.divizoom@gmail.com>
2021-06-13 12:23:02 +0600

    solve the link problem

--- wp-login.php
+++ wp-login.php
@@ -92,7 +92,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/wordpress\/wp-admin\/admin-ajax.php"}};
 <script type="text/javascript" src="https://Divizoom.github.io/layout/wp-includes/js/wp-util.min.js?ver=5.7.2" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /* <![CDATA[ */
-var userProfileL10n = {"user_id":"0","nonce":"e50273bb49"};
+var userProfileL10n = {"user_id":"0","nonce":"0af23296d4"};
 /* ]]> */
 </script>
 <script type="text/javascript" id="user-profile-js-translations">
